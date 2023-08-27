aef0f155f1ac1fc841518f72a470206583eae556
Siddhant J <sidbnjadhav@gmail.com>
2023-08-27 13:12:25 +0530

    code push

---
+++ Q1.py
@@ -0,0 +1,28 @@
+import psycopg2, sys, os
+f = open('./player.txt','r')
+name = f.read()
+#name = name + "%" query for like 
+
+
+database = 'flis'
+user = 'postgres'
+password = 'sidjadhav0602'
+port = '5432'
+host = 'localhost'
+
+conn = None
+try:
+    conn = psycopg2.connect(database=database,user=user,password=password,port=port,host=host)
+    cur = conn.cursor()
+    cur.execute("select name from players where name =%s",(name,))
+    #query for is Like : 
+    # cur.execute("select name from players where name like %s ",(name,))
+    result = cur.fetchall()
+    for i in result:
+        print(i[0])
+    cur.close()
+except(Exception,psycopg2.DatabaseError) as error:
+    print(error)
+finally:
+    if conn is not None:
+        conn.close()
---
+++ Q2.py
@@ -0,0 +1,28 @@
+f= open('./name.txt')
+f_name = f.read()
+
+
+database = 'university'
+user = 'postgres'
+password = 'sidjadhav0602'
+port = '5432'
+host = 'localhost'
+
+conn = None
+try: 
+    conn = psycopg2.connect(database=database,user=user,password=password,port=port,host=host)
+    print("Connection is Established!")
+
+    cur = conn.cursor()
+    cur.execute("select roll_no from students where student_fname= %s",(f_name,))
+
+    result = cur.fetchall()
+    for i in result:
+        print(i[0])
+    
+    cur.close()
+except(Exception,psycopg2.DatabaseError) as error:
+    print(error)
+finally:
+    if conn is not None:
+        conn.close()
